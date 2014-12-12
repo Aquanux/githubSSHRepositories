15b25437413b1fa9c18acb1959ee7d3a29a157a8
devGitBlit <akuanux@gmail.com>
2014-12-12 14:19:49 +0100

    Push..Push !!

--- GitHub-3-HelloMavenWebAppSSH/src/main/java/com/gitub/maven/ssh/HelloSSH.java
+++ GitHub-3-HelloMavenWebAppSSH/src/main/java/com/gitub/maven/ssh/HelloSSH.java
@@ -6,7 +6,7 @@ public class HelloSSH {
 		
 		System.out.println("Hello Maven on GitHub SSH Key with a Dynamic Web App !!");
 		
-		System.out.println("PUSH !!");
+		System.out.println("PUSH...PUSH !! Ca va marcher cette foic-ci ??");
 		
 	}
 
